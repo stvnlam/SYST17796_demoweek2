1d6da57f04c7c8aaa76917a99564d022c8be1351
stevenlam <lamsteve@sheridancollege.ca>
2019-05-23 09:43:54 -0400

    new branch called part time

---
+++ src/studentdemoweek1/PartTimeStudent.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentdemoweek1;
+
+/**
+ *
+ * @author steve
+ */
+public class PartTimeStudent extends Student {
+    
+}
